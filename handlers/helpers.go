6d402562a0dd1f608a310a3227cafd86d8ec3043
guilherme <guile.hm@hotmail.com>
2022-04-16 14:36:33 -0300

    create helpers file

---
+++ handlers/helpers.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HandleApiErrors(w http.ResponseWriter, status int, message string) {
+	if message == "" {
+		message = http.StatusText(status)
+	}
+
+	jsonResponse, _ := json.Marshal(struct {
+		Error string `json:"error"`
+	}{message})
+	w.WriteHeader(status)
+	w.Write(jsonResponse)
+}
