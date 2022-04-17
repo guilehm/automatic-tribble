2cd694864973308eb9b34f7cff40211dbd40f128
guilherme <guile.hm@hotmail.com>
2022-04-17 14:02:59 -0300

    add email to user struct

--- models/models.go
+++ models/models.go
@@ -5,5 +5,6 @@ import "time"
 type User struct {
 	ID         int       `json:"id"`
 	Name       string    `json:"name"`
+	Email      string    `json:"email"`
 	DateJoined time.Time `json:"date_joined"`
 }
