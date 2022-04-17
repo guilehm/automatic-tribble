313988910b2350385b93605fe517e3b5c0525841
guilherme <guile.hm@hotmail.com>
2022-04-17 14:02:32 -0300

    update migration, create index for email

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -1,5 +1,9 @@
-CREATE TABLE users (
-    id serial PRIMARY KEY,
-    name varchar(50) NOT NULL UNIQUE,
+CREATE TABLE users
+(
+    id          serial PRIMARY KEY,
+    name        varchar(50)              NOT NULL,
+    email       varchar                  NOT NULL,
     date_joined timestamp with time zone NOT NULL
-)
+);
+
+CREATE UNIQUE INDEX email_unique_idx on users (LOWER(email));
