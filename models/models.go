762e0cbdcc78c04c6eff669665b1236806dbadf8
guilherme <guile.hm@hotmail.com>
2022-04-23 13:24:47 -0300

    add required for date joined field

--- models/models.go
+++ models/models.go
@@ -6,5 +6,5 @@ type User struct {
 	ID         int       `json:"id"`
 	Name       string    `json:"name" validate:"required,gte=3"`
 	Email      string    `json:"email" validate:"required,email"`
-	DateJoined time.Time `json:"date_joined"`
+	DateJoined time.Time `json:"date_joined" validate:"required"`
 }
