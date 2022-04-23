9b199a5a25c5a098a21374b22ff09010ea65cb8f
guilherme <guile.hm@hotmail.com>
2022-04-23 11:06:52 -0300

    add validators to user struct

--- models/models.go
+++ models/models.go
@@ -4,7 +4,7 @@ import "time"
 
 type User struct {
 	ID         int       `json:"id"`
-	Name       string    `json:"name"`
-	Email      string    `json:"email"`
+	Name       string    `json:"name" validate:"required,gte=3"`
+	Email      string    `json:"email" validate:"required,email"`
 	DateJoined time.Time `json:"date_joined"`
 }
