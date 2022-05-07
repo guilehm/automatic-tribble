3db1f5e512c27cb41978c90848c041731b1d6f56
guilherme <guile.hm@hotmail.com>
2022-05-07 13:52:59 -0300

    update migration

--- db/migrations/000001_create_users_table.up.sql
+++ db/migrations/000001_create_users_table.up.sql
@@ -2,11 +2,11 @@ CREATE TABLE users
 (
     id            serial PRIMARY KEY,
     name          varchar(50)              NOT NULL,
-    email         varchar                  NOT NULL,
-    date_joined   timestamp with time zone NOT NULL,
+    email         varchar(512)             NOT NULL,
     password      varchar(256)             NOT NULL,
     token         varchar(256)             NOT NULL,
-    refresh_token varchar(256)             NOT NULL
+    refresh_token varchar(256)             NOT NULL,
+    date_joined   timestamp with time zone NOT NULL
 );
 
 CREATE UNIQUE INDEX email_unique_idx on users (LOWER(email));
