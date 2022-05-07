182c9b526193b9cfbe867ea4efe8f7b41f2ad91f
guilherme <guile.hm@hotmail.com>
2022-05-07 11:46:36 -0300

    add password and tokens to user struct

--- models/models.go
+++ models/models.go
@@ -3,10 +3,13 @@ package models
 import "time"
 
 type User struct {
-	ID         int       `json:"id"`
-	Name       string    `json:"name" validate:"required,gte=3"`
-	Email      string    `json:"email" validate:"required,email"`
-	DateJoined time.Time `json:"date_joined" validate:"required"`
+	ID           int       `json:"id"`
+	Name         string    `json:"name" validate:"required,gte=3"`
+	Email        string    `json:"email" validate:"required,email"`
+	DateJoined   time.Time `json:"date_joined" validate:"required"`
+	Password     string    `json:"-"`
+	Token        string    `json:"token"`
+	RefreshToken string    `json:"refresh_token"`
 }
 
 type Player struct {
