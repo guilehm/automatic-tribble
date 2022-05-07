fed546cab5f1379f3596a3b6bb185ae9bb34ba66
guilherme <guile.hm@hotmail.com>
2022-05-07 14:11:35 -0300

    update user model

--- models/models.go
+++ models/models.go
@@ -6,10 +6,10 @@ type User struct {
 	ID           int       `json:"id"`
 	Name         string    `json:"name" validate:"required,gte=3"`
 	Email        string    `json:"email" validate:"required,email"`
-	DateJoined   time.Time `json:"date_joined" validate:"required"`
-	Password     string    `json:"-"`
+	Password     string    `json:"password" validate:"required,gte=5"`
 	Token        string    `json:"token"`
 	RefreshToken string    `json:"refresh_token"`
+	DateJoined   time.Time `json:"date_joined"`
 }
 
 type Player struct {
