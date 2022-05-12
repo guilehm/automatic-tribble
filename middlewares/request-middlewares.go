8363bd2328ec62fe99317944476721efa1fbc8ac
guilherme <guile.hm@hotmail.com>
2022-05-12 17:40:00 -0300

    create middleware for setting headers

--- middlewares/request-middlewares.go
+++ middlewares/request-middlewares.go
@@ -11,3 +11,9 @@ func LogRequest(handler http.Handler) http.Handler {
 		handler.ServeHTTP(w, r)
 	})
 }
+func SetHeaders(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		handler.ServeHTTP(w, r)
+	})
+}
