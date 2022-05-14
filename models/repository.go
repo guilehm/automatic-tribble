6f0bfabe067a939ed367fa3de37043c88a3405eb
guilherme <guile.hm@hotmail.com>
2022-05-14 14:11:59 -0300

    update repository file

--- models/repository.go
+++ models/repository.go
@@ -5,7 +5,10 @@ import "context"
 type UserRepository interface {
 	GetUser(ctx context.Context, ID int) (*User, error)
 	GetUserList(ctx context.Context) ([]*User, error)
-	SaveUser(ctx context.Context, user User) (*User, error)
+	CreateUser(ctx context.Context, user User) (*User, error)
 	UpdateUser(ctx context.Context, user User) (*User, error)
-	DeleteUser(ctx context.Context, ID int) (*User, error)
+	DeleteUser(ctx context.Context, ID int) error
+
+	GetUserByEmail(ctx context.Context, email string) (*User, error)
+	UpdateUserTokens(ctx context.Context, ID int, token, refresh string) error
 }
