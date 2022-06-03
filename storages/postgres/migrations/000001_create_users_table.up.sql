1ed15ba6b9bbeb85b91e41577f2b5e2408b53038
guilherme <guile.hm@hotmail.com>
2022-06-03 20:25:26 -0300

    update migration, set unique constraint into email allowing null values

--- storages/postgres/migrations/000001_create_users_table.up.sql
+++ storages/postgres/migrations/000001_create_users_table.up.sql
@@ -1,14 +1,12 @@
-CREATE EXTENSION IF NOT EXISTS citext;
-
 CREATE TABLE users
 (
     id            serial PRIMARY KEY,
     username      varchar(50)              NOT NULL,
-    email         CITEXT                   NOT NULL,
+    email         varchar(512) UNIQUE,
     password      varchar(256)             NOT NULL,
     token         varchar(256)             NOT NULL,
     refresh_token varchar(256)             NOT NULL,
     date_joined   timestamp with time zone NOT NULL
 );
 
-CREATE UNIQUE INDEX username_unique_users_idx on users (LOWER(username));
+CREATE UNIQUE INDEX name_unique_users_idx on users (LOWER(username));
