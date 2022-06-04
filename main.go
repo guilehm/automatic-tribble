5620b21e1316b4c7351f13ae9db4d3db37698a27
guilherme <guile.hm@hotmail.com>
2022-06-04 10:13:23 -0300

    add validate username url to router

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 	r.HandleFunc("/users/", handlers.GetUserList).Methods("GET")
 	r.HandleFunc("/users/{id}/", handlers.GetUserDetail).Methods("GET")
 	r.HandleFunc("/users/", handlers.CreateUser).Methods("POST")
+	r.HandleFunc("/users/username/validate/", handlers.ValidateUsername).Methods("POST")
 
 	r.HandleFunc("/users/", middlewares.Authentication(handlers.UpdateUser)).Methods("PUT")
 	r.HandleFunc("/users/", middlewares.Authentication(handlers.DeleteUser)).Methods("DELETE")
